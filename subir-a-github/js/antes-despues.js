1deea3296bc779d53ec0399c6d79c0c9c4ef67fc
claudi-droid <claudi@curiaprojects.com>
2026-09-24 00:07:47 +0200

    Delete subir-a-github directory

--- subir-a-github/js/antes-despues.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll('.ad input[type=range]').forEach(function(r){
-  var s=function(){r.parentElement.style.setProperty('--pos',r.value+'%')};r.addEventListener('input',s);s();});
